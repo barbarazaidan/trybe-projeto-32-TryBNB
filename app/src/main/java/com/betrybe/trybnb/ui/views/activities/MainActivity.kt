f6bd0db656f9dd26414be2206a02e5370ae4d443
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2024-02-19 15:34:26 -0300

    Initial commit

---
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.betrybe.trybnb.ui.views.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.betrybe.trybnb.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
