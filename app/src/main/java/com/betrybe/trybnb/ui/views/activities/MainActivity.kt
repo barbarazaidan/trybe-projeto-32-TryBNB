89d1da7ee6e36ff1ce8d4dc297fb02e516ca54b2
Bárbara <babivaldez@gmail.com>
2024-03-04 16:10:27 -0300

    inicia o projeto

--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import com.betrybe.trybnb.R
 
 class MainActivity : AppCompatActivity() {
 
+    // inicia o projeto
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
