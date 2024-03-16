de96f36935d6ed5b3b8dd0e50f0bcd275d0886e4
Bárbara <babivaldez@gmail.com>
2024-03-15 21:38:41 -0300

    faz as validações do fragmento referente à criação de uma reserva

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/CreateReservationFragment.kt
@@ -13,13 +13,13 @@ import com.google.android.material.textfield.TextInputLayout
 
 class CreateReservationFragment : Fragment() {
 
-    private lateinit var firstName: TextInputLayout
-    private lateinit var lastName: TextInputLayout
-    private lateinit var checkinDate: TextInputLayout
-    private lateinit var checkoutDate: TextInputLayout
-    private lateinit var additionalInfo: TextInputLayout
-    private lateinit var totalPrice: TextInputLayout
-    private lateinit var deposit: CheckBox
+    private lateinit var inputFirstName: TextInputLayout
+    private lateinit var inputLastName: TextInputLayout
+    private lateinit var inputCheckinDate: TextInputLayout
+    private lateinit var inputCheckoutDate: TextInputLayout
+    private lateinit var inputAdditionalInfo: TextInputLayout
+    private lateinit var inputTotalPrice: TextInputLayout
+    private lateinit var inputDeposit: CheckBox
     private lateinit var button: Button
 
     override fun onCreateView(
@@ -28,15 +28,73 @@ class CreateReservationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_create_reservation, container, false)
-        firstName = view.findViewById(R.id.first_name_create_reservation)
-        lastName = view.findViewById(R.id.last_name_create_reservation)
-        checkinDate = view.findViewById(R.id.checkin_create_reservation)
-        checkoutDate = view.findViewById(R.id.checkout_create_reservation)
-        additionalInfo = view.findViewById(R.id.additional_needs_create_reservation)
-        totalPrice = view.findViewById(R.id.total_price_create_reservation)
-        deposit = view.findViewById(R.id.depositpaid_create_reservation)
+        inputFirstName = view.findViewById(R.id.first_name_create_reservation)
+        inputLastName = view.findViewById(R.id.last_name_create_reservation)
+        inputCheckinDate = view.findViewById(R.id.checkin_create_reservation)
+        inputCheckoutDate = view.findViewById(R.id.checkout_create_reservation)
+        inputAdditionalInfo = view.findViewById(R.id.additional_needs_create_reservation)
+        inputTotalPrice = view.findViewById(R.id.total_price_create_reservation)
         button = view.findViewById(R.id.create_reservation_button)
 
+        button.setOnClickListener {
+            handleLogin()
+        }
+
         return view
+    }
+
+    private fun handleLogin() {
+        val firstName = inputFirstName.editText?.text.toString()
+        val lastName = inputLastName.editText?.text.toString()
+        val checkin = inputCheckinDate.editText?.text.toString()
+        val checkout = inputCheckoutDate.editText?.text.toString()
+        val additionalInfo = inputAdditionalInfo.editText?.text.toString()
+        val totalPrice = inputTotalPrice.editText?.text.toString()
+
+        isValidCredentials(firstName, lastName, checkin, checkout, additionalInfo, totalPrice)
+    }
+
+    private fun isValidCredentials(
+        firstName: String,
+        lastName: String,
+        checkin: String,
+        checkout: String,
+        additionalInfo: String,
+        totalPrice: String
+    ) {
+        if (firstName.isEmpty()) {
+            inputFirstName.error = "O campo Nome é obrigatório"
+        } else {
+            inputFirstName.error = null
+        }
+        if (lastName.isEmpty()) {
+            inputLastName.error = "O campo Sobrenome é obrigatório"
+        } else {
+            inputLastName.error = null
+        }
+
+        if (checkin.isEmpty()) {
+            inputCheckinDate.error = "O campo Checkin é obrigatório"
+        } else {
+            inputCheckinDate.error = null
+        }
+
+        if (checkout.isEmpty()) {
+            inputCheckoutDate.error = "O campo Checkout é obrigatório"
+        } else {
+            inputCheckoutDate.error = null
+        }
+
+        if (additionalInfo.isEmpty()) {
+            inputAdditionalInfo.error = "O campo Necessidades Adicionais é obrigatório"
+        } else {
+            inputAdditionalInfo.error = null
+        }
+
+        if (totalPrice.isEmpty()) {
+            inputTotalPrice.error = "O campo Preço Total é obrigatório"
+        } else {
+            inputTotalPrice.error = null
+        }
     }
 }
